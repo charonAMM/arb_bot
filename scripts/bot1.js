4b5153da79194e608eaf4b81c8b07cf0529c6ac9
Nicholas Fett <nicholasfett@Nicholass-MacBook-Pro.local>
2023-05-01 11:23:13 -0400

    init

---
+++ scripts/bot1.js
@@ -0,0 +1,13 @@
+//run bot in loop
+
+//make sure you have X amount of baseToken and x amount of chd for arb opportunity
+
+// if you don't, use pool to rebalance or request funding (e.g. via a deposit or whatever)
+
+//check prices of three assets
+
+// assume CHD price is average of three chains
+
+// trade in/out of CHD based on this info to rebalance pools w/in some threshold (e.g. must be 1% off) 
+
+//TODO: check dripAmount and do stuff
---
+++ scripts/bot2.js
@@ -0,0 +1,10 @@
+//bot for grabbing rewards in the system
+/*types of rewards:
+-oracleRewards
+-userRewards
+*/
+
+//grab gas price for network
+//grab price of each asset
+//grab given reward
+//calculate if profitable, then perform action
